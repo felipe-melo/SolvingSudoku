8a53a4625977f78d8b79cf7882f202d2253e8677
Lucas Ribeiro Ferreira <lucas@arcolabs.com.br>
2017-02-08 20:51:59 -0200

    no message

--- SimulatedAnnealing.jl
+++ SimulatedAnnealing.jl
@@ -5,7 +5,7 @@ module SimulatedAnnealing
   function fitness(solution::Solution)
     local fit = 0
     for i in 1:solution.order
-      @show fit += size(find(y -> length(find(x-> x == y, solution.grid[i,:])) > 1, solution.grid[i,:]))[1]
+      fit += size(find(y -> length(find(x-> x == y, solution.grid[i,:])) > 1, solution.grid[i,:]))[1]
     end
     for j in 1:solution.order
       #Analise se existe o número i repetido na coluna j
@@ -24,35 +24,40 @@ module SimulatedAnnealing
       after_fit = solution.fitness
       temp_delta[i] = abs(after_fit - prev_fit)
     end
-
+    @show temp_delta
     @show return std(temp_delta)
   end
 
   function neighborhoodWalk(solution::Solution, game::Game)
     new_solution = deepcopy(solution)
     local possib_size = 0
-    @show game
     indexs = Int[]
     sorted_grid = Array{Int, Int}
+    new_solution_sorted_grid = Array{Int, Int}
     line_end, colum_end, colum_start, line_start = 0,0,0,0
     while (possib_size < 2 )
       @show sorted_grid_line, sorted_grid_colum = rand(1:game.order), rand(1:game.order)
-
       line_end = sorted_grid_line * game.order
       colum_end = sorted_grid_colum * game.order
       line_start = line_end - game.order + 1
       colum_start = colum_end - game.order + 1
       sorted_grid = game.grid[line_start:line_end,colum_start:colum_end]
+      new_solution_sorted_grid = new_solution.grid[line_start:line_end,colum_start:colum_end]
 
-      @show indexs = find(x-> x==0, sorted_grid)
+      indexs = find(x-> x==0, sorted_grid)
       possib_size = length(indexs)
     end
     shuffle!(indexs)
     first_index = pop!(indexs)
     second_index = pop!(indexs)
-    @show sorted_grid[first_index], sorted_grid[second_index] = sorted_grid[second_index], sorted_grid[first_index]
-    new_solution.grid[line_start:line_end,colum_start:colum_end] = sorted_grid
-    @show new_solution.fitness = fitness(new_solution)
+    println("Solução Velha")
+    printSolution(solution)
+    new_solution_sorted_grid[first_index], new_solution_sorted_grid[second_index] = new_solution_sorted_grid[second_index], new_solution_sorted_grid[first_index]
+    new_solution.grid[line_start:line_end,colum_start:colum_end] = new_solution_sorted_grid
+    new_solution.fitness = fitness(new_solution)
+    println("Solução Nova")
+    printSolution(new_solution)
+    @show new_solution.fitness
     return new_solution
   end
 
@@ -62,40 +67,51 @@ module SimulatedAnnealing
     t = zeros(0) #vetor de temperaturas com 0 elementos
     probality(delta) = exp(-(delta/t[end])) # função de probabilidade
     ml = length(findnz(game.grid))^2 # ml of Markov Chain = quantidade de valores não fixos ao quadrado
+    @show ml
     ml_time = 0 # quantidade de Markov Chains percorridas = 0
-    @show t0 = initialTemperature(game, 100)
-    push!(t, t0) # inicializa solução inicial com a variação padrão de um quantidade pequena de vizinhanças
     solution = game.solutions[1] # solução atual
     solution.fitness = fitness(solution)
     best_solution = game.solutions[1] # melhor solução
-    time = 0 # tempo = 0; numero de interações
 
+    t0 = initialTemperature(game, 100)
+    @show t0
+    push!(t, t0) # inicializa solução inicial com a variação padrão de um quantidade pequena de vizinhanças
+    time = 0 # tempo = 0; numero de interações
     while solution.fitness > 0 && time < max_num_of_interation
-      @show new_solution = neighborhoodWalk(solution, game) # produz nova solução
+      new_solution = neighborhoodWalk(solution, game) # produz nova solução
+      @show new_solution.fitness
+      @show solution.fitness
       @show delta = new_solution.fitness - solution.fitness # calcula a varuação do fitness
-      if delta > 0 # verifica se o fitness melhorou
+      if delta < 0 # verifica se o fitness melhorou
         best_solution = new_solution # atualiza melhor solução
-        solutions = new_solution # solução atual
+        solution = new_solution # solução atual
       else # Se não melhorou
-        @show prob = probality(delta) # calcula a probalidade de aceitação
+        @show prob = probality(abs(delta)) # calcula a probalidade de aceitação
+
         if rand() < prob # se aceitar
-          @show solution = new_solution # atualiza solução atual
+          @show solution.grid - new_solution.grid
+          solution = new_solution # atualiza solução atual
         end
       end
 
       if time % ml == 0 # se percorri toda a Markov Chain
-        @show push!(t, a * t[end] ) # atualizo temperatura
+        @show time, ml,
+        push!(t, a * t[end] ) # atualizo temperatura
         ml_time += 1
       end
 
       if ml_time == reheat_th ## se percorri todas as n cadeias e não achei a solução...
-        @show push!(t, t[1]) # reseta temperatura para a temperatura inicial
+        @show ml_time, reheat_th
+        push!(t, t[1]) # reseta temperatura para a temperatura inicial
         ml_time = 0 # reseta o contador de markov chains
       end
-      @show time += 1;
+      time += 1;
+      @show time
     end
 
-    @show game.solutions[1] = best_solution
+    game.solutions[1] = best_solution
+    @show game.solutions[1].fitness
+    @show game.solutions[1].grid
   end
 
   export runSimulatedAnnealing, neighborhoodWalk
--- Support.jl
+++ Support.jl
@@ -5,17 +5,34 @@ include("CreateInstances.jl")
 include("FillObvious.jl")
 include("SimulatedAnnealing.jl")
 
+using Types
 using CreateInstances
 using FillObvious
 using SimulatedAnnealing
+using Util
 using Iterators
 
-order = convert(Int32, 3)
+order = convert(Int32, 4)
 quant = convert(Int32, 1)
 p = convert(Float64, 0.3)
 game = generateConfiguration(order, p)
 makeFirstSolution(game, quant)
 runSimulatedAnnealing(game)
 
-t = zeros(Int, 0)
-push!(t, 2)
+println("SOLUÇÃO")
+print(game.solutions[1])
+
+
+
+
+function fitness(solution::Solution)
+  local fit = 0
+  for i in 1:solution.order
+    fit += size(find(y -> length(find(x-> x == y, solution.grid[i,:])) > 1, solution.grid[i,:]))[1]
+  end
+  for j in 1:solution.order
+    #Analise se existe o número i repetido na coluna j
+    fit += size(find(y -> length(find(x-> x == y, solution.grid[:,j])) > 1, solution.grid[:,j]))[1]
+  end
+  return fit
+end
