dec0484b1df4be452ecd1371b217f1b5f40be797
Lucas Ribeiro Ferreira <lucas@arcolabs.com.br>
2017-02-08 16:45:12 -0200

    fix obvious errors

--- Support.jl
+++ Support.jl
@@ -5,12 +5,17 @@ include("CreateInstances.jl")
 include("FillObvious.jl")
 include("SimulatedAnnealing.jl")
 
-importall CreateInstances
-importall FillObvious
-importall SimulatedAnnealing
+using CreateInstances
+using FillObvious
+using SimulatedAnnealing
+using Iterators
 
 order = convert(Int32, 3)
 quant = convert(Int32, 1)
 p = convert(Float64, 0.3)
 game = generateConfiguration(order, p)
 makeFirstSolution(game, quant)
+runSimulatedAnnealing(game)
+
+t = zeros(Int, 0)
+push!(t, 2)
