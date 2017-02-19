19577335f5b8458834cd6c98d8ec9add58743fce
Lucas Ribeiro Ferreira <lucas_rferreira@live.com>
2017-02-19 14:49:33 -0300

    Otimização do Algotimo de Simulated Anealing para rodar mais rápido

--- SimulatedAnnealing.jl
+++ SimulatedAnnealing.jl
@@ -14,25 +14,24 @@ module SimulatedAnnealing
     return fit
   end
 
-  function initialTemperature(game::Game, num_of_samples::Int = 100)
+  function initialTemperature(game::Game, possibIndexs::Array{Array{Int}}, num_of_samples::Int = 100)
     solution = deepcopy(game.solutions[1])
     temp_delta = zeros(Int, num_of_samples)
 
     for i in 1:num_of_samples
       prev_fit = solution.fitness
-      solution = neighborhoodWalk(solution, game)
+      solution = neighborhoodWalk(solution, game, possibIndexs)
       after_fit = solution.fitness
       temp_delta[i] = after_fit - prev_fit
     end
     return std(temp_delta)
   end
 
-  function neighborhoodWalk(solution::Solution, game::Game)
+  function neighborhoodWalk(solution::Solution, game::Game, possibIndexs::Array{Array{Int}})
     new_solution = deepcopy(solution)
     local possib_size = 0
-    indexs = Int[]
-    sorted_grid = Array{Int, Int}
     new_solution_sorted_grid = Array{Int, Int}
+    indexs = Array{Int}
     line_end, colum_end, colum_start, line_start = 0,0,0,0
     while (possib_size < 2 )
       sorted_grid_line, sorted_grid_colum = rand(1:game.order), rand(1:game.order)
@@ -40,10 +39,10 @@ module SimulatedAnnealing
       colum_end = sorted_grid_colum * game.order
       line_start = line_end - game.order + 1
       colum_start = colum_end - game.order + 1
-      sorted_grid = game.grid[line_start:line_end,colum_start:colum_end]
+
       new_solution_sorted_grid = new_solution.grid[line_start:line_end,colum_start:colum_end]
 
-      indexs = find(x-> x==0, sorted_grid)
+      indexs = copy(possibIndexs[sorted_grid_line, sorted_grid_colum])
       possib_size = length(indexs)
     end
     shuffle!(indexs)
@@ -55,6 +54,21 @@ module SimulatedAnnealing
     return new_solution
   end
 
+  function calcPossibIndexs(game::Game)
+    possibIndexs = Array(Array{Int}, game.order, game.order)
+    for j in 1:game.order
+      for i in 1:game.order
+        line_end = i * game.order
+        colum_end = j * game.order
+        line_start = line_end - game.order + 1
+        colum_start = colum_end - game.order + 1
+        atual_sub_matrix = game.grid[line_start:line_end,colum_start:colum_end]
+
+        possibIndexs[i,j] = find(x-> x==0, atual_sub_matrix)
+      end
+    end
+    return possibIndexs
+  end
   function runSimulatedAnnealing(game::Game, a::Float64 = 0.9, reheat_th::Int = 1000, max_num_of_interation::Int = 500000)
     initial_timestamp =  Int(now())
     ## Inicializa Variáveis
@@ -68,30 +82,31 @@ module SimulatedAnnealing
     time = 0 # tempo = 0; numero de interações
     timestamp = Int(now()) - initial_timestamp
     if solution.fitness > 0
-      t0 = initialTemperature(game, 100)
+      possibIndexs = calcPossibIndexs(game)
+      t0 = initialTemperature(game, possibIndexs, 100)
       t = t0 # inicializa solução inicial com a variação padrão de um quantidade pequena de vizinhanças
       timestamp = Int(now()) - initial_timestamp
       println("$(timestamp) $(ml) $(solution.fitness) $(t)")
       while solution.fitness > 0 && time < max_num_of_interation
-        new_solution = neighborhoodWalk(solution, game) # produz nova solução
+        new_solution = neighborhoodWalk(solution, game, possibIndexs) # produz nova solução
         delta = new_solution.fitness - solution.fitness # calcula a varuação do fitness
         if delta < 0 # verifica se o fitness melhorou
           best_solution = new_solution # atualiza melhor solução
           solution = new_solution # solução atual
         else # Se não melhorou
           prob = probality(abs(delta)) # calcula a probalidade de aceitação
-  
+
           if rand() < prob # se aceitar
             solution.grid - new_solution.grid
             solution = new_solution # atualiza solução atual
           end
         end
-  
+
         if time % ml == 0 # se percorri toda a Markov Chain
           ml_time += 1
           t = a * t # atualizo temperatura
         end
-  
+
         if ml_time == reheat_th ## se percorri todas as n cadeias e não achei a solução...
           t = t0 # reseta temperatura para a temperatura inicial
           ml_time = 0 # reseta o contador de markov chains
